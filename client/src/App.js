0e6d22b0f610f25714d857984ceefc86c148903e
diganta15 <ddas461598@gmail.com>
2021-07-17 13:56:01 +0530

    Added context for Auth

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,26 @@ import { BrowserRouter as Router,Route, Switch } from "react-router-dom";
 import { Home } from "./components/pages/Home";
 import { About } from "./components/pages/About";
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 function App() {
 	return (
-		<ContactState>
-		<Router>
-			<Fragment>
-				<NavBar />
-				<div className="container">
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/about" component={About} />
+		<AuthState>
+			<ContactState>
+				<Router>
+					<Fragment>
+						<NavBar />
+						<div className="container">
+							<Switch>
+								<Route exact path="/" component={Home} />
+								<Route exact path="/about" component={About} />
 
-					</Switch>
-				</div>
-			</Fragment>
-		</Router>
-		</ContactState>
+							</Switch>
+						</div>
+					</Fragment>
+				</Router>
+			</ContactState>
+		</AuthState>
+		
 	);
 }
 
