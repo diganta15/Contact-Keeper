ccd945a0352a2de6e51f08f388e978ef3bb2c6fd
diganta15 <ddas461598@gmail.com>
2021-07-20 16:03:41 +0530

    Added Private routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { NavBar } from "./components/layout/NavBar";
 import { BrowserRouter as Router,Route, Switch } from "react-router-dom";
 import { Home } from "./components/pages/Home";
 import { About } from "./components/pages/About";
+import PrivateRoute from "./components/routing/PrivateRoute";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alerts from "./components/layout/Alerts";
@@ -25,7 +26,7 @@ function App() {
 							<div className="container">
 								<Alerts />
 								<Switch>
-									<Route exact path="/" component={Home} />
+									<PrivateRoute exact path="/" component={Home} />
 									<Route exact path="/about" component={About} />
 									<Route exact path="/register" component={Register} />
 									<Route exact path="/login" component={Login} />
